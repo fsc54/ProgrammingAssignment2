3a836d4f9aa5cd4a26875bc2dfbc4632e308c39c
fsc54 <francescha54@gmail.com>
2015-09-27 14:33:41 -0600

    programming assignment 2 - uploaded for peer assessment

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
 ## Programming Assigment #2:
-## UPLOADED FOR PEER ASSESSMENT
+## UPLOADED FOR PEER ASSESSMENT 
 ##     1. makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 ##     2. cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
 ##          If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
